03905ec08a11f92b415a6fa166cb05283754ae3f
Guillermo Guerrero <wolf.fox1985@gmail.com>
2015-03-14 08:43:11 +0100

    Incomplete script in GO.

---
+++ main.go
@@ -0,0 +1,192 @@
+package main
+
+import (
+  "fmt"
+  "encoding/csv"
+  "time"
+  "os"
+  "bytes"
+  "strings"
+  "strconv"
+  "math"
+  "encoding/json"
+  "encoding/base64"
+  "compress/zlib"
+)
+
+func exist_file(file_name string) bool {
+  if _, err := os.Stat("domain_spread_cache"); err != nil {
+    if os.IsNotExist(err) {
+      fmt.Println("Error ==> file not found ./domain_spread_cache")
+      return false
+    }
+  }
+
+  return true
+}
+
+func uncompress_block(block string) string {
+  decoded, _ := base64.StdEncoding.DecodeString(block)
+	b := bytes.NewReader(decoded)
+
+	reader, _ := zlib.NewReader(b)
+  buf := new(bytes.Buffer)
+  buf.ReadFrom(reader)
+  s := buf.String() // Does a complete copy of the bytes in the buffer.
+
+  reader.Close()
+
+  return s
+}
+
+func process_file(file_name string) {
+  var arr_pageviews_facebook []int
+  avg_pageviews_facebook := 0
+  num_pageviews_facebook := 0
+
+  var hash_unique_users_facebook map[string]bool
+  hash_unique_users_facebook = make(map[string]bool)
+
+  var hash_unique_users_facebook_per_day map[string]map[string]bool
+  hash_unique_users_facebook_per_day = make(map[string]map[string]bool)
+
+  var hash_users_google_per_day map[string]interface{}
+  hash_users_google_per_day = make(map[string]interface{})
+
+  csvfile, err := os.Open(file_name)
+
+  if err != nil {
+     fmt.Println(err)
+     return
+  }
+
+  defer csvfile.Close()
+  reader := csv.NewReader(csvfile)
+
+  reader.Comma = '\t'
+  reader.FieldsPerRecord = -1 // see the Reader struct information below
+  rawCSVdata, err := reader.ReadAll()
+
+  if err != nil {
+     fmt.Println(err)
+     os.Exit(1)
+  }
+
+  for _, row := range rawCSVdata {
+    domain := row[0]
+    date := row[1]
+    block := row[2]
+
+    strJSON := uncompress_block(block)
+
+    var data interface{}
+    json.Unmarshal([]byte(strJSON), &data)
+    user_traces := data.(map[string]interface{})
+
+
+    if strings.Contains(domain, "facebook.com") {
+      _, exist := hash_unique_users_facebook_per_day[date]
+      if !exist {
+        hash_unique_users_facebook_per_day[date] = make(map[string]bool)
+      }
+
+      for user_id, times_interface := range user_traces {
+        hash_unique_users_facebook[user_id] = true
+        hash_unique_users_facebook_per_day[date][user_id] = true
+
+        times := times_interface.(map[string]interface{})
+        for _, arr_sec_pageviews_interface := range times {
+          arr_sec_pageviews := arr_sec_pageviews_interface.([]interface {})
+
+          arr_pageviews_facebook = append(arr_pageviews_facebook, int(arr_sec_pageviews[1].(float64)))
+          avg_pageviews_facebook += int(arr_sec_pageviews[1].(float64))
+          num_pageviews_facebook += 1
+        }
+      }
+    }
+
+
+    if strings.Contains(domain, "google.") {
+      _, exist := hash_users_google_per_day[date]
+      if !exist {
+        hash_users_google_per_day[date] = make(map[string][]int)
+      }
+
+      for user_id, times_interface := range user_traces {
+        hash_unique_users_facebook[user_id] = true
+        hash_unique_users_facebook_per_day[date][user_id] = true
+
+        times := times_interface.(map[string]interface{})
+        for time_slice_str, arr_sec_pageviews_interface := range times {
+          time_slice, _ := strconv.ParseInt(time_slice_str, 0, 64)
+          if time_slice >= 20*4 && time_slice < 23*4 {
+            fmt.Printf("Date and domain => %s - %s\n", time_slice, arr_sec_pageviews_interface)
+            //hash_users_google_per_day[date] = append(hash_users_google_per_day[date], arr_sec_pageviews_interface)
+          }
+        }
+      }
+    }
+
+    fmt.Printf("Date and domain => %s - %s\n", domain, date)
+  }
+
+
+  fmt.Printf("\n\n")
+  fmt.Printf("=====================\n")
+  fmt.Printf("-------RESULTS-------\n")
+  fmt.Printf("=====================\n")
+
+  fmt.Printf("1. Unique users, per day, on facebook.com:\n")
+  for date, users := range hash_unique_users_facebook_per_day {
+    fmt.Printf("Date: %s -- users: %d\n", date, len(users))
+  }
+
+  fmt.Printf("2. Per­day average number of seconds spent on Google properties (google.*) between 20:00 and 23:00, by people that also have visited facebook.com.\n")
+  // hash_users_google_per_day.each do |date, arr_users_secs|
+  //   average = 0
+  //   counter = 0
+  //
+  //   arr_users_secs.each do |user_sec|
+  //     if hash_unique_users_facebook.has_key? user_sec[0]
+  //       average += user_sec[1].to_i
+  //       counter += 1
+  //     end
+  //   end
+  //
+  //   average /= counter.to_f if counter > 0
+  //   puts "Date: #{date} -- avg seconds: #{average}"
+  // end
+
+
+  fmt.Printf("3. Calculate the standard deviation of the amount of pageviews on facebook.com, based on all data.\n")
+  if num_pageviews_facebook > 0 {
+    avg_pageviews_facebook /= num_pageviews_facebook
+  }
+
+  sum_squares := float64(0)
+  for amount := range arr_pageviews_facebook {
+    diff := amount - avg_pageviews_facebook
+    sum_squares += float64(math.Pow(float64(diff), float64(2)))
+  }
+
+  std_pageviews_facebook := float64(0)
+  // if num_pageviews_facebook > 0 {
+  //   std_pageviews_facebook := math.Sqrt(float64(sum_squares)/float64(num_pageviews_facebook))
+  // }
+
+  fmt.Printf("Avg pageviews: %d\n", avg_pageviews_facebook)
+  fmt.Printf("Std pageviews: %d\n", std_pageviews_facebook)
+}
+
+func main() {
+  program_starts := time.Now()
+  fmt.Println("Begin ===>>", program_starts)
+
+  if exist_file("domain_spread_cache") {
+    process_file("domain_spread_cache")
+  }
+
+  program_ends := time.Now()
+  fmt.Println("End ===>>", program_ends)
+  fmt.Println("Time in seconds =>", time.Since(program_starts))
+}
